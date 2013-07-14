c3e27a28ff1ae97f5d33fa686755abc53d9d3bb6
Reza Pourrad <pourrad@yahoo.com>
2013-07-14 20:32:36 +0000

    Updated grader.js to get urls

--- grader.js
+++ grader.js
@@ -23,9 +23,49 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var sys = require('util'),
+    rest = require('restler');
 var cheerio = require('cheerio');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://secure-harbor-1522.herokuapp.com/";
+var INHTML_DEFAULT = "HTML.html"
+
+var getURL = function(inurl,outhtml) {
+
+var url = inurl;
+//var htmlfile = outhtml;
+
+var url = inurl.toString();
+var htmlfile = outhtml.toString();
+
+console.log("URL:",url);
+console.log("OUT:",htmlfile);
+
+rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    fs.writeFileSync(htmlfile, result);
+  }
+});
+
+}; 
+
+var assertURLExists = function(inurl) {
+    var instr = inurl.toString();
+    rest.get(instr).on('complete', function(result) {
+      if (result instanceof Error) {
+          console.log("There was an error in reading %s . Exiting.", instr);
+          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  } else {
+          return instr;
+   }
+});
+};
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +105,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url  <url_path>', 'URL Path to index.html', INHTML_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+      var inurl = new String();
+      //console.log(program.checks);
+      //console.log(program.url);
+      inurl = program.url;
+
+      //console.log(inurl);
+
+      getURL(inurl,INHTML_DEFAULT); 
+    
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
